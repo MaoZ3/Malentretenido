ce9b7c529d15d8af36c45485d2c88ae1f0a928d7
MaoZ3 <117559028+MaoZ3@users.noreply.github.com>
2023-12-21 16:22:03 -0300

    movimientos player
    movimientos player y granada

--- Malentretenido/script/Impostor.gd
+++ Malentretenido/script/Impostor.gd
@@ -1,5 +1,40 @@
 extends CharacterBody2D
 
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = direction < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
+
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+	if not is_on_floor():
+		$AnimatedSprite2D.play("jump")
+		
+	move_and_slide()
+	
+	
+	
+	"""extends CharacterBody2D
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -12,17 +47,38 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$AnimatedSprite2D. play("jump")
 		velocity.y = JUMP_VELOCITY
-
+		
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D. play("run")
+		$AnimatedSprite2D.flip_v = false
+	# See the note below about boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D. play("idle")
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false	
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
+	
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	
+
+	move_and_slide()"""
+	
+	
